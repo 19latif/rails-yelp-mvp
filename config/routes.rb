bc476e1b984faba68469e818f551dbeadf735f58
Latif Turaan <latiftoran@gmail.com>
2023-02-22 19:42:42 +0100

    rails yelp

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "articles#index
+  get '/restaurants', to: 'restaurants#index'
+  get '/restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  post '/restaurants', to: 'restaurants#create'
+  get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.create(name: "curry_rice", category: "japanese", address: "curry_road", phone_number: "123345")
+Restaurant.create(name: "muler", category: "chinese", address: "muller_strase", phone_number: "234562")
+Restaurant.create(name: "boner", category: "chinese", address: "bon_road", phone_number: "345123")
+Restaurant.create(name: "kabuli", category: "chinese", address: "kabul-road", phone_number: "7342516")
+Restaurant.create(name: "parischicken", category: "belgian", address: "oliv_road", phone_number: "2349876")
